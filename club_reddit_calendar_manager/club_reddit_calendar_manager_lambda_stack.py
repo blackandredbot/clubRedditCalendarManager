c29dd19b15671b5e44467aeb2570091e97e2df1f
Michael Foley <folem@amazon.com>
2023-03-27 13:19:48 -0400

    CodePipeline Self-mutation complete. Pushing Lambda with Layers.

--- club_reddit_calendar_manager/club_reddit_calendar_manager_lambda_stack.py
+++ club_reddit_calendar_manager/club_reddit_calendar_manager_lambda_stack.py
@@ -10,15 +10,15 @@ class ClubRedditCalendarManagerLambdaStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # club_reddit_calendar_manager_layer = _lambda.LayerVersion(
-        #     self,
-        #     "ClubRedditLambdaLayer",
-        #     removal_policy=RemovalPolicy.RETAIN,
-        #     code=_lambda.Code.from_asset(
-        #         "lambda/club_reddit_calendar_manager_layer.zip"
-        #     ),
-        #     compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
-        # )
+        club_reddit_calendar_manager_layer = _lambda.LayerVersion(
+            self,
+            "ClubRedditLambdaLayer",
+            removal_policy=RemovalPolicy.RETAIN,
+            code=_lambda.Code.from_asset(
+                "lambda/club_reddit_calendar_manager_layer.zip"
+            ),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
+        )
 
         # The code that defines your stack goes here
         EventbridgeToLambda(
@@ -28,7 +28,7 @@ class ClubRedditCalendarManagerLambdaStack(Stack):
                 code=_lambda.Code.from_asset("lambda"),
                 runtime=_lambda.Runtime.PYTHON_3_9,
                 handler="update_calendar.handler",
-                # layers=[club_reddit_calendar_manager_layer],
+                layers=[club_reddit_calendar_manager_layer],
             ),
             event_rule_props=events.RuleProps(
                 schedule=events.Schedule.rate(Duration.minutes(5))
