5093437482058aa368fc0aa35692be4c4904e399
Michael Foley <folem@amazon.com>
2023-03-27 20:21:45 -0400

    Correcting namespace conflicts

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ application_prefix = "ClubRedditCalendarManager"
 
 ClubRedditCalendarManagerStack(
     app,
-    f"{ClubRedditCalendarManager}Stack",
+    f"{application_prefix}Stack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
